22f0e591581228160da8e8891d024eb2e6119865
John Velasco <velascoj@ksu.edu>
2021-06-09 16:44:54 -0500

    Add project files.

---
+++ CivicRecBot/SignIn.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CivicRecBot
+{
+    public class SignIn 
+    {
+        public void Entry(IWebDriver driver)
+        {
+            driver.FindElement(By.Id("login-username"));
+        }
+    }
+}
